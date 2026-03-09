8e858e2d0d95c3599027900e1945381bad6d1b72
ludek.kolarcik <98560219+kolarcik@users.noreply.github.com>
2026-03-09 09:24:26 +0100

    Revert "feat: scaffold MAUI project structure"
    This reverts commit 6bd7ebc59a7deebee96362fa29924e053b03d2ca.

--- src/PingPong.App/Services/IAudioService.cs
+++
@@ -1,8 +0,0 @@
-namespace PingPong.App.Services;
-
-public interface IAudioService
-{
-    Task PlayAsync(string soundName);
-    Task StopAllAsync();
-    float Volume { get; set; }
-}
--- src/PingPong.App/Services/IPreferencesService.cs
+++
@@ -1,8 +0,0 @@
-namespace PingPong.App.Services;
-
-public interface IPreferencesService
-{
-    T Get<T>(string key, T defaultValue);
-    void Set<T>(string key, T value);
-    void Remove(string key);
-}
